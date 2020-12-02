b66edca3f5da8211316d9e55b3ec0fe578b34d75
Liam Bai <liambai@liams-mbp-2.lan>
2020-12-02 10:25:07 -0500

    multiple plots

--- hh_euler.py
+++ hh_euler.py
@@ -18,11 +18,11 @@ def hh_euler(I, tmax, v, mi, hi, ni):
     iterations = math.ceil(tmax/dt)
 
     g_Na = 120
-    e_Na = 115
+    v_Na = 115
     g_K = 36
-    e_K = -12
+    v_K = -12
     g_L = 0.3
-    e_L = 10.6
+    v_L = 10.6
 
     # Initializing variable arrays
     t = np.linspace(0,tmax,num=iterations)
@@ -36,37 +36,46 @@ def hh_euler(I, tmax, v, mi, hi, ni):
     h[0]=hi
     n[0]=ni
 
-    #Euler method
+    # Euler's method
     for i in range(iterations-1):
-        V[i+1] = V[i] + dt*(g_Na*m[i]**3*h[i]*(e_Na-(V[i]+65)) + g_K*n[i]**4*(e_K-(V[i]+65)) + \
-            g_L*(e_L-(V[i]+65)) + I)
-        m[i+1] = m[i] + dt*(a_M(V[i])*(1-m[i]) - b_M(V[i])*m[i])
-        h[i+1] = h[i] + dt*(a_H(V[i])*(1-h[i]) - b_H(V[i])*h[i])
-        n[i+1] = n[i] + dt*(a_N(V[i])*(1-n[i]) - b_N(V[i])*n[i])
-
-    plt.plot(t, V)
-    plt.xlabel('Time')
-    plt.ylabel('Membrane Potential')
-    plt.title('Membrane Potential vs. Time')
-    plt.show()
+        curr_v = V[i] + 65
+        V[i+1] = V[i] + dt*(g_Na*m[i]**3*h[i]*(v_Na-(curr_v)) + g_K*n[i]**4*(v_K-(curr_v)) + \
+            g_L*(v_L-(curr_v)) + I)
+        m[i+1] = m[i] + dt*(a_M(curr_v)*(1-m[i]) - b_M(curr_v)*m[i])
+        h[i+1] = h[i] + dt*(a_H(curr_v)*(1-h[i]) - b_H(curr_v)*h[i])
+        n[i+1] = n[i] + dt*(a_N(curr_v)*(1-n[i]) - b_N(curr_v)*n[i])
+    return t, V
 
 # alpha and beta functions for the gating variables 
-def a_M(V):
-    return (2.5-0.1*(V+65)) / (np.exp(2.5-0.1*(V+65)) -1)
+def a_M(v):
+    return 0.1*(25 - v)/(np.exp((25-v)/10) - 1)
 
-def b_M(V):  
-    return 4*np.exp(-(V+65)/18)
+def b_M(v):
+    return 4*np.exp(-1*v/18)
 
-def a_H(V):
-    return 0.07*np.exp(-(V+65)/20)
+def a_H(v):
+    return 0.07*np.exp(-1*v/20)
 
-def b_H(V):
-    return 1./(np.exp(3.0-0.1*(V+65))+1)
+def b_H(v):
+    return 1/(np.exp((30-v)/10) + 1)
 
-def a_N(V):
-    return (0.1-0.01*(V+65)) / (np.exp(1-0.1*(V+65)) -1)
+def a_N(v):
+    return 0.01 * (10 - v)/(np.exp((10-v)/10) - 1)
 
-def b_N(V):
-    return 0.125*np.exp(-(V+65)/80)
+def b_N(v):
+    return 0.125*np.exp(-1*v/80)
 
-hh_euler(0.08, 40, -65, 0.4, 0.2, 0.5)
+
+if __name__ == "__main__":
+    I_0 = [1, 5, 10, 25]
+    sub_pos = [(0,0),(0,1),(1,0),(1,1)]
+    fig, axs = plt.subplots(2, 2, constrained_layout=True)
+    for i in range(4):
+        t, V = hh_euler(I_0[i], 100, -65, 0.4, 0.2, 0.5)
+        pos = sub_pos[i]
+        axs[pos].plot(t, V)
+        axs[pos].set(xlabel='t', ylabel='V')
+        axs[pos].set_title(str(I_0[i]) + ' mV')
+        axs[pos].set_ylim([-80, 40])
+    plt.suptitle('Membrane Potential vs. Time for Varying Stimulus Currents', fontsize=18)
+    plt.show()
